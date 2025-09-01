73219a220387efc7ca0e02badeeea39d6e2cc59c
Rohit Yadav <0112CS221112@gmail.com>
2025-09-01 09:44:03 +0530

    Update urls.py

--- fotoflow/urls.py
+++ fotoflow/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
+#all_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),  
     path("accounts/", include("allauth.urls")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
